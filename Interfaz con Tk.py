0235dfc852b35be458813d43d1579583c907e143
salvaH21 <sholivares90@gmail.com>
2023-11-25 18:47:59 +0100

    interfaz finalizada

--- Interfaz con Tk.py
+++ Interfaz con Tk.py
@@ -9,77 +9,94 @@ def salida():
     raiz.destroy()
     
 def crear():
-    def pulsar():
-        texto=campo1.get()
-        mensaje1.destroy()
-        campo1.destroy()
-        boton1.destroy()
-        añadir(texto)
-
-    def añadir(tipodelista):
-        mensaje2 = tk.Label(raiz,text=("Lista de "+tipodelista))
-        mensaje2.pack(padx=20,pady=10)
-        mensaje3.pack(padx=20,pady=10)
-        campo2.pack()
-        boton2.pack(padx=20,pady=20)
-        boton3.pack(padx=20,pady=20)
-
-    def pulsarAñadir():
-        texto=campo2.get()
-        lista.append(texto)
-        print(lista)
-        campo2.delete(0,tk.END)
-
-    def pulsarAceptar():
-        mensaje3.destroy()
-        campo2.destroy()
-        boton2.destroy()
-        boton3.destroy()
-        mensaje4 = tk.Label(raiz,text="Lista creada correctamente")
+    def aceptar():
+        nombre = campo1.get()
+        apellidos = campo2.get()
+        edad = selector.get()
         mensaje4.pack(padx=20,pady=20)
-        print(lista)
+        mensaje1.destroy()
+        mensaje2.destroy()
+        mensaje3.destroy()
+        campo1.destroy()
+        campo2.destroy()
+        selector.destroy()
+        caja1.destroy()
+        boton1.destroy()
+        boton2.destroy()
+        print(nombre,apellidos,edad)
 
-    lista = []
-    mensaje1 = tk.Label(raiz,text="De qué quieres hacer la lista")
-    mensaje1.pack(padx=20,pady=20)
-    mensaje3 = tk.Label(raiz,text="Añade un elemento")
+    def borrar():
+        campo1.delete(0,tk.END)
+        campo2.delete(0,tk.END)
+        selector.delete(0,tk.END)
+        caja1.deselect()
+
+    mensaje1 = tk.Label(raiz,text="Nombre")
+    mensaje1.pack(padx=10,pady=10)
     campo1 = tk.Entry(raiz)
     campo1.pack()
+    mensaje2 = tk.Label(raiz,text=("Apellidos"))
+    mensaje2.pack(padx=10,pady=10)
     campo2 = tk.Entry(raiz)
-    boton1 = tk.Button(raiz,text="Aceptar",command=pulsar)
-    boton1.pack(padx=20,pady=20)
-    boton2 = tk.Button(raiz,text="Añadir",command=pulsarAñadir)
-    boton3 = tk.Button(raiz,text="Aceptar",command=pulsarAceptar)
-    return lista
+    campo2.pack()
+    mensaje3 = tk.Label(raiz,text=("Edad"))
+    mensaje3.pack(padx=10,pady=10)
+    selector = tk.Spinbox(raiz,from_=0,to=100)
+    selector.pack()
+    caja1 = tk.Checkbutton(raiz,text="Tengo coche")
+    caja1.pack()
+    boton1 = tk.Button(raiz,text="Aceptar",command=aceptar)
+    boton1.pack(padx=10,pady=10)
+    boton2 = tk.Button(raiz,text="Borrar",command=borrar)
+    boton2.pack(padx=10,pady=10)
 
 def abrir():
-    print(listado)
-    
+    def añadir():
+        texto = campo1.get()
+        lista.append(texto)
+        campo1.delete(0,tk.END)
+        print(lista)
+
+    def mostrarlista():
+        mensaje2 = tk.Label(raiz,text="Tus aficiones son")
+        mensaje2.pack(padx=10,pady=10)
+        listado = tk.Listbox(raiz)
+        for aficion in lista:
+            listado.insert(tk.END,aficion)
+        listado.pack()
+        
+    mensaje4.destroy()
+    lista = []
+    mensaje1 = tk.Label(raiz,text="Escribe tus aficiones")
+    mensaje1.pack(padx=10,pady=10)
+    campo1 = tk.Entry(raiz)
+    campo1.pack()
+    boton1 = tk.Button(raiz,text="Añadir",command=añadir)
+    boton1.pack(padx=10,pady=10)
+    boton2 = tk.Button(raiz,text="Aceptar",command=mostrarlista)
+    boton2.pack(padx=10,pady=10)
 
 #título de la ventana
 raiz.title("Título provisional")
-#declaramos la lista
-listado = crear()
+#Declaración mensaje 4
+mensaje4 = tk.Label(raiz,text="Lista creada correctamente")
 #Creamos una barra de menu
 barramenu = tk.Menu(raiz)
 raiz.config(menu=barramenu)
 
-lista = tk.Menu(barramenu,tearoff=0)
-barramenu.add_cascade(label="Lista",menu=lista)
+encuesta = tk.Menu(barramenu,tearoff=0)
+barramenu.add_cascade(label="Encuesta",menu=encuesta)
 
-lista.add_command(label="Crear nuevo formulario",command=crear)
-lista.add_command(label="Abrir una lista",command=abrir)
-lista.add_command(label="Eliminar una lista")
+encuesta.add_command(label="Nueva encuesta",command=crear)
+encuesta.add_command(label="Lista de aficiones",command=abrir)
 
 salir = tk.Menu(barramenu,tearoff=0)
 barramenu.add_cascade(label="Salir",menu=salir)
 
 salir.add_command(label="Salir",command=salida)
 
-
-
 #Dimesiones de la ventana y localización en pantalla
-anchuraventana = 400
+anchuraventana = 300
 alturaventana = 400
 anchurapantalla =raiz.winfo_screenwidth()
 alturapantalla = raiz.winfo_screenheight()
@@ -88,24 +105,5 @@ esquinay = int(alturapantalla/2 - alturaventana/2)
 raiz.geometry(str(anchuraventana)+"x"+str(alturaventana)+"+"+str(esquinax)+"+"+str(esquinay))
 #cambio de icono de la ventana
 raiz.iconbitmap("icono.ico")
-###---------------------------------------------------
-###label
-##tk.Label(raiz,text="Hola mundo desde tkinter").pack(padx=50,pady=50)
-###boton
-##tk.Button(raiz,text="Botón"",command=definir un metodo para poerlo aquí").pack(padx=50,pady=50)
-###campo de entrada
-##tk.Entry(raiz).pack(padx=50,pady=50)
-###checkbutton
-##tk.Checkbutton(raiz,text="marcar").pack(padx=50,pady=50)
-###radiobutton
-##tk.Radiobutton(raiz,text="marcar").pack(padx=50,pady=50)
-###lista con elementos
-##frutas = ['manzana','pera','plátano','fresa']
-##listado = tk.Listbox(raiz)
-##for fruta in frutas:
-##    listado.insert(tk.END,fruta)
-##listado.pack(padx=50,pady=50)
-
-
 
 raiz.mainloop()
